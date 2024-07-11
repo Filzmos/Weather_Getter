dce31d99133fa6b78bbd82e792334e795a07c533
filzmos <alexander.filzmoser0000@gmail.com>
2024-07-11 22:02:26 +0200

    Commit

---
+++ main.py
@@ -0,0 +1,42 @@
+# https://openweathermap.org/api/geocoding-api
+# https://www.youtube.com/watch?v=bKCORrHbutQ
+
+import requests
+from datetime import datetime
+
+kelvin = 273.15
+
+print("Current Weather\nFrom which city do you want to know the weather?")
+city = input()
+print("\nCurrently Loading......\n")
+response_location = requests.get(
+    "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=a643535e40a80c84e1505cc90ed5eb95")
+# print(response_location.status_code) # 200 means succesfull connection
+# print(response_location.json())
+if response_location.status_code == 200:
+    print("Getting Location successful\n")
+else:
+    print("Getting Location unsuccessful\n")
+latitude = response_location.json()[0]['lat']
+longitude = response_location.json()[0]['lon']
+# print(latitude)
+# print(longitude)
+
+response_weather = requests.get("https://api.openweathermap.org/data/2.5/weather?lat=" + str(latitude) + "&lon=" + str(
+    longitude) + "&appid=a643535e40a80c84e1505cc90ed5eb95")
+# print(response_weather.status_code)
+# print(response_weather.json())
+if response_weather.status_code == 200:
+    print("Getting Weather successful\n")
+else:
+    print("Getting Weather unsuccessful\n")
+
+print("Weather: " + response_weather.json()['weather'][0]['main'])
+print("Description: " + response_weather.json()['weather'][0]['description'])
+print("Temperature: " + str(round(response_weather.json()['main']['temp'] - kelvin, 2)) + "°C")
+print("Temperature feels like: " + str(round(response_weather.json()['main']['feels_like'] - kelvin, 2)) + "°C")
+print("Humidity: " + str(response_weather.json()['main']['humidity']) + "%")
+
+print("Timezone(From UTC Time: " + str(int(response_weather.json()['timezone'] / 3600)) + ")")
+print("Current time: " + str(datetime.now().strftime('%H:%M:%S')))
+print("Current Date: " + str(datetime.now().date()))
